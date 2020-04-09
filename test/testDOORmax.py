0a12768427b3999cf37c75d12e532029110b96c6
borea17 <markus.norden@campus.tu-berlin.de>
2020-04-09 22:37:16 +0200

    added travis support

--- test/testDOORmax.py
+++ test/testDOORmax.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from efficient_rl.agents import DOORmax
 from efficient_rl.environment import TaxiEnvironment
 from efficient_rl.environment.oo_mdp import OOTaxi
+import numpy as np
 
 
 class testDOORmax(TestCase):
@@ -28,4 +29,116 @@ class testDOORmax(TestCase):
             env.reset()
 
             rewards, step_times = agent.main(env, max_steps=testDOORmax.max_steps)
+        return
+
+    def test_useless_actions_do_not_occur_in_DOORmax(self):
+        print('Test: useless actions do not occur in DOORmax')
+        for agent, env in zip(self.agents, self.oo_envs):
+            print(' ', env)
+
+            agent.reset()
+            env.reset()
+
+            done = False
+            state = env.s
+
+            last_state, last_action = [None], [None]
+            for i_step in range(testDOORmax.max_steps):
+                action = agent.step(state)
+
+                if np.all(last_state[0] == state[0]):  # last action had no effect => bad action
+                    # taking the same action would be useless
+                    self.assertTrue(last_action != action)
+                new_state, reward, done, _ = env.step(action)
+                if not agent.transition_learner_can_predict(state, action):
+                    agent.add_experience_to_transition_learner(state, action, new_state)
+                if not agent.reward_learner_can_predict(state, action):
+                    agent.add_experience_to_reward_learner(state, action, reward)
+                agent.update_emp_MDP(state, action)
+
+                last_state, last_action = state, action
+
+                state = new_state
+                if done:
+                    break
+        return
+
+    def test_predictions_are_correct(self):
+        print('Test: predictions are correct in DOORmax')
+        for agent, env in zip(self.agents, self.oo_envs):
+            print(' ', env)
+
+            agent.reset()
+            env.reset()
+
+            done = False
+            state = env.s
+            for i_step in range(testDOORmax.max_steps):
+                action = agent.step(state)
+                new_state, reward, done, _ = env.step(action)
+                if not agent.transition_learner_can_predict(state, action):
+                    agent.add_experience_to_transition_learner(state, action, new_state)
+                else:
+                    new_state_pred = agent.predict_transition(state, action)
+                    self.assertTrue(np.all(new_state_pred == new_state[0]))
+                if not agent.reward_learner_can_predict(state, action):
+                    agent.add_experience_to_reward_learner(state, action, reward)
+                else:
+                    expected_immediate_reward = agent.predict_expected_immediate_reward(state, action)
+                    self.assertTrue(expected_immediate_reward == reward)
+                agent.update_emp_MDP(state, action)
+                # predictions should be possible now:
+                if agent.transition_learner_can_predict(state, action) and \
+                   agent.reward_learner_can_predict(state, action):
+                    # assert transition prediction
+                    new_state_pred = agent.predict_transition(state, action)
+                    self.assertTrue(np.all(new_state_pred == new_state[0]))
+                    # assert reward prediction
+                    expected_immediate_reward = agent.predict_expected_immediate_reward(state, action)
+                    self.assertTrue(expected_immediate_reward == reward)
+                else:
+                    self.assertTrue(1 == 0)
+                state = new_state
+
+                if done:
+                    break
+        return
+
+    def test_dropoff_and_pickup_do_not_occur_twice_in_non_reward_state(self):
+        print('Test: dropoff and pickup do not occur twice in non reward state (DOORmax)')
+        for agent, env in zip(self.agents, self.oo_envs):
+            print(' ', env)
+
+            agent.reset()
+            env.reset()
+
+            visited_non_reward_dropoff_states = []
+            visited_non_reward_pickup_states = []
+
+            state = env.s
+            for i_step in range(testDOORmax.max_steps):
+                action = agent.step(state)
+
+                if action == 4:  # ensure that corresponding non reward state has not been seen
+                    self.assertTrue(state[0] not in visited_non_reward_pickup_states)
+                if action == 5:
+                    self.assertTrue(state[0] not in visited_non_reward_dropoff_states)
+
+                new_state, reward, done, _ = env.step(action)
+
+                if action == 4 and reward == -10:  # bad Pickup action
+                    visited_non_reward_pickup_states.append(state[0])
+                if action == 5 and reward == -10:  # bad Dropfoff action
+                    visited_non_reward_dropoff_states.append(state[0])
+
+                if not agent.transition_learner_can_predict(state, action):
+                    agent.add_experience_to_transition_learner(state, action, new_state)
+                if not agent.reward_learner_can_predict(state, action):
+                    agent.add_experience_to_reward_learner(state, action, reward)
+
+                agent.update_emp_MDP(state, action)
+
+                state = new_state
+                if done:
+                    break
         return
